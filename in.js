7aa05d1cb8af47a7733418d9cf50a4bc725fe5bd
Zxw <1046137644@qq.com>
2019-10-27 15:42:01 +0800

    webpack配置

--- in.js
+++ in.js
@@ -1,3 +1,7 @@
 import com from './main'
 
 com();
+alert(112);
+var element = document.getElementById('app');
+element.innerHTML='hello webpack';
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,32 @@
 const path = require('path')
+const HtmlwebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    entry: {
+        app: './in.js',
+    },
+    //      {
+    //     main: './main.js'//文件入口
+    // },
+    output: {
+        filename: "build.js",//文件名
+        path: path.resolve(__dirname, 'dist')//路径
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'css-loader',
+                    'style-loader'
+                ]
+
+            },
+            //     {
+            //         test: /\.(js|jsx)$/,
+            //         use: 'babel-loader'
+            //     }
+        ]
+    }
+}
